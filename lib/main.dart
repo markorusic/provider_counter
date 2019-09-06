109d066c02ec008cbd9bf3a4478839712ee79ded
Marko Rusic <markousp5@gmail.com>
2019-09-06 18:34:56 +0200

    counter app example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'widgets/app.dart';
 
-void main() => runApp(App());
+void main() {
+  runApp(ChangeNotifierProvider(
+    // Initialize the model in the builder. That way, Provider
+    // can own Counter's lifecycle, making sure to call `dispose`
+    // when not needed anymore.
+    builder: (context) => Counter(),
+    child: MyApp(),
+  ));
+}
--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,19 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
-class App extends StatelessWidget {
+class Counter with ChangeNotifier {
+  int value = 0;
+
+  void increment() {
+    value += 1;
+    notifyListeners();
+  }
+
+  void decrement() {
+    value -= 1;
+    notifyListeners();
+  }
+
+  void reset() {
+    value = 0;
+    notifyListeners();
+  }
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Provider Example',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Counter counterModel = Provider.of<Counter>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
       ),
-      home: Scaffold(
-        body: Container(
-          child: Center(
-            child: Text('Provider Example'),
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Consumer<Counter>(
+              builder: (context, counter, child) => Text(
+                'Value: ${counter.value}',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.refresh,
+                    size: 30,
+                  ),
+                  onPressed: () => counterModel.reset(),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    size: 30,
+                  ),
+                  onPressed: () => counterModel.increment(),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.remove,
+                    size: 30,
+                  ),
+                  onPressed: () => counterModel.decrement(),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
